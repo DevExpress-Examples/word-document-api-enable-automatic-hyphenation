698c71afd13404e3a899dccc0bc09b918d9d3f88
Poline Fedorova <polina.fedorova@devexpress.com>
2019-08-19 11:30:20 +0300

    add files

---
+++ CS/word-processing-hyphenation/Program.cs
@@ -0,0 +1,47 @@
+﻿using DevExpress.XtraRichEdit;
+using DevExpress.XtraRichEdit.API.Native;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace word_processing_hyphenation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (RichEditDocumentServer wordProcessor = new RichEditDocumentServer())
+            {
+                //Register the created service implementation
+                wordProcessor.LoadDocument("Multimodal.docx");
+
+                //Load embedded dictionaries
+                var openOfficePatternStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("word_processing_hyphenation.hyphen.dic");
+                var customDictionaryStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("word_processing_hyphenation.hyphen_exc.dic");
+
+                //Create dictionary objects
+                OpenOfficeHyphenationDictionary hyphenationDictionary = new OpenOfficeHyphenationDictionary(openOfficePatternStream, new System.Globalization.CultureInfo("EN-US"));
+                CustomHyphenationDictionary exceptionsDictionary = new CustomHyphenationDictionary(customDictionaryStream, new System.Globalization.CultureInfo("EN-US"));
+
+                //Add them to the word processor's collection
+                wordProcessor.HyphenationDictionaries.Add(hyphenationDictionary);
+                wordProcessor.HyphenationDictionaries.Add(exceptionsDictionary);
+
+                //Specify hyphenation settings
+                wordProcessor.Document.Hyphenation = true;
+                wordProcessor.Document.HyphenateCaps = true;
+
+                //Export the result to the PDF format
+                wordProcessor.ExportToPdf("Result.pdf");
+
+            }
+            //Open the result
+            Process.Start("Result.pdf");
+
+        }
+    }
+}
---
+++ VB/word-processing-hyphenation/Program.vb
@@ -0,0 +1,43 @@
+﻿Imports DevExpress.XtraRichEdit
+Imports DevExpress.XtraRichEdit.API.Native
+Imports System
+Imports System.Collections.Generic
+Imports System.Diagnostics
+Imports System.Linq
+Imports System.Reflection
+Imports System.Text
+Imports System.Threading.Tasks
+
+Namespace word_processing_hyphenation
+	Friend Class Program
+		Shared Sub Main(ByVal args() As String)
+			Using wordProcessor As New RichEditDocumentServer()
+				'Register the created service implementation
+				wordProcessor.LoadDocument("Multimodal.docx")
+
+				'Load embedded dictionaries
+				Dim openOfficePatternStream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("hyphen.dic")
+				Dim customDictionaryStream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("hyphen_exc.dic")
+
+				'Create dictionary objects
+				Dim hyphenationDictionary As New OpenOfficeHyphenationDictionary(openOfficePatternStream, New System.Globalization.CultureInfo("EN-US"))
+				Dim exceptionsDictionary As New CustomHyphenationDictionary(customDictionaryStream, New System.Globalization.CultureInfo("EN-US"))
+
+				'Add them to the word processor's collection
+				wordProcessor.HyphenationDictionaries.Add(hyphenationDictionary)
+				wordProcessor.HyphenationDictionaries.Add(exceptionsDictionary)
+
+				'Specify hyphenation settings
+				wordProcessor.Document.Hyphenation = True
+				wordProcessor.Document.HyphenateCaps = True
+
+				'Export the result to the PDF format
+				wordProcessor.ExportToPdf("Result.pdf")
+
+			End Using
+			'Open the result
+			Process.Start("Result.pdf")
+
+		End Sub
+	End Class
+End Namespace
